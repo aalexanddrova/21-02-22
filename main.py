85b066212ccebeeac28940757eb3c29db92efbf0
Anastasia Doroshenko <91866423+aalexanddrova@users.noreply.github.com>
2022-02-21 07:03:29 +0000

    2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-x = range(1,7)
-for elem in x:
-  n = pow(3,elem)
-  print(n)
+text=input("ievadi vardu:")
+for elem in range(1,23):
+  play=22*(text+" ")
+  if elem>+0:
+    print(text)
+    print(elem,text)
+    print(play)
